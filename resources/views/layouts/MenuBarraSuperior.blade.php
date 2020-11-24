3e6aa4d6d3701d3c0fb9bf57c2d5539c3faa0401
miguel alaniz <miguelalaniz77@gmail.com>
2020-11-23 21:26:05 -0600

    Intento 1 de foto de perfil

--- resources/views/layouts/MenuBarraSuperior.blade.php
+++ resources/views/layouts/MenuBarraSuperior.blade.php
@@ -46,31 +46,27 @@
                         <div class="widget-content-left">
                             <div class="btn-group">
                                 <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="p-0 btn">
-                                    <img width="42" class="rounded-circle"  src= {{ asset('assets/images/avatars/1.jpg') }} alt="">
+                                    <img width="42" class="rounded-circle"  src= {{ asset(Auth::user()->foto) }} alt="">
                                     <i class="fa fa-angle-down ml-2 opacity-8"></i>
                                 </a>
                                 <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
-                                    <button type="button" tabindex="0" class="dropdown-item">User Account</button>
-                                    <button type="button" tabindex="0" class="dropdown-item">Settings</button>
-                                    <h6 tabindex="-1" class="dropdown-header">Header</h6>
-                                    <button type="button" tabindex="0" class="dropdown-item">Actions</button>
+                                    <h6 tabindex="-1" class="dropdown-header">Mi perfil</h6>
                                     <div tabindex="-1" class="dropdown-divider"></div>
-                                    <button type="button" tabindex="0" class="dropdown-item">Dividers</button>
+                                    <a href="{{route('profile.show')}}" tabindex="0" class="dropdown-item">Configuracion</a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                        @csrf
+                                        <button type="summit" tabindex="0" class="dropdown-item" value="logout" >Cerrar sesión</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                         <div class="widget-content-left  ml-3 header-user-info">
                             <div class="widget-heading">
-                                Alina Mclourd
+                                {{Auth::user()->name}}
                             </div>
                             <div class="widget-subheading">
-                                VP People Manager
+                                Recetas: {{Auth::user()->Recetas->count()}}
                             </div>
-                        </div>
-                        <div class="widget-content-right header-user-info ml-3">
-                            <button type="button" class="btn-shadow p-1 btn btn-primary btn-sm show-toastr-example">
-                                <i class="fa text-white fa-calendar pr-1 pl-1"></i>
-                            </button>
                         </div>
                     </div>
                 </div>
