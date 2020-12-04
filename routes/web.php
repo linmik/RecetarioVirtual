2752c07afa870b8f5fc7b2ad77468baad0381ede
miguel alaniz <miguelalaniz77@gmail.com>
2020-12-03 20:25:14 -0600

    No se que hay en este commit XD

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProcesoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecetaController;
 use App\Models\Proceso;
+use App\Models\Receta;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,10 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/recetas/{Receta}/ingredientes',[RecetaController::class,'ingredientes'])->name('recetas.ingrediente')->middleware('auth');
+Route::post('/recetas/{Receta}/ingredientes',[RecetaController::class,'agregarIngrediente'])->name('recetas.agregarIngrediente')->middleware('auth');
+Route::delete('/recetas/{Receta}/{Ingrediente}',[RecetaController::class,'eliminarIngrediente'])->name('recetas.eliminarIngrediente')->middleware('auth');
 
 Route::get('/recetas/{Receta}/agregarProceso',[RecetaController::class,'agregarProcesos'])->name('recetas.agregarProcesos')->middleware('auth');
 Route::resource('/recetas',RecetaController::class)->middleware('auth');
