c905318fe194d1bfa58b9eb9820058f50aca36b2
Miguel Angel Alaniz Palomera <50251893+linmik@users.noreply.github.com>
2020-12-04 14:18:31 -0600

    user campo tipo (#4)
    soft delete de recetas
    imagen en barra superior

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,8 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
-            $table->string('foto')->default('assets/images/sinFoto.png');
+            $table->string('tipo')->default('usuario');
+            //$table->string('foto')->default('assets/images/sinFoto.png');
             $table->timestamps();
         });
     }
