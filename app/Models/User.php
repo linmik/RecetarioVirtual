81184615988b114462ce065a47d9b4feb95dbd7a
Ramón Franco Malta <ramonfrancomalta@gmail.com>
2020-12-04 18:26:37 -0600

    Implementacion de mensaje y de autentificacion con correo

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 use App\Models\Seguidor;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
---
+++ resources/views/partials/mensaje.blade.php
@@ -0,0 +1,10 @@
+@if(session()->has('mensaje'))
+
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
+        <button type="button" class="close" aria-label="Close" data-dismiss="alert">
+            <span aria-hidden="true">×</span>
+        </button>
+        {{session('mensaje')}}
+    </div>
+
+@endif
